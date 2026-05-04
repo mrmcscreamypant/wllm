96dc19dd4d29fc1c1aa743b8d07e4a60d4d5b867
mrmcscreamypant <161328176+mrmcscreamypant@users.noreply.github.com>
2026-05-04 20:21:05 +0000

    moderate css

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-import { CreateWebWorkerMLCEngine } from "@mlc-ai/web-llm";
+import * as wllm from "@mlc-ai/web-llm";
+import showdown from 'showdown';
 import * as xterm from '@xterm/xterm';
+//@ts-ignore
+import "./index.css";
+//@ts-ignore
 import '@xterm/xterm/css/xterm.css';
+import { FitAddon } from "@xterm/addon-fit";
 
-const term = new xterm.Terminal({fontFamily: "'monospace'"});
-term.open(document.getElementById("output"));
+const fitAddon = new FitAddon();
+const term = new xterm.Terminal({ fontFamily: "monospace" });
+term.loadAddon(fitAddon);
+//@ts-ignore
+term.open(document.getElementById("loading-progress"));
+fitAddon.fit();
 
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register(
@@ -12,31 +21,33 @@ if ("serviceWorker" in navigator) {
     );
 }
 
+document.getElementById("loading-progress")?.addEventListener("resize", () => fitAddon.fit());
+
+const converter = new showdown.Converter({ ghCodeBlocks: true, omitExtraWLInCodeBlocks: true, smoothLivePreview: true });
 
 async function runWorker() {
-    const engine = await CreateWebWorkerMLCEngine(
+    const engine = await wllm.CreateWebWorkerMLCEngine(
         new Worker(new URL("./worker.ts", import.meta.url), { type: "module" }),
         "Llama-3.2-1B-Instruct-q4f32_1-MLC", { initProgressCallback: (progress: any) => { term.writeln(progress.text) } }
     );
 
-    const messages = [
-        { role: "system", content: "You are currently running in WASM on a school chromebook." },
-        { role: "user", content: "Hello!" },
+    const messages: wllm.ChatCompletionMessageParam[] = [
+        { role: "system", content: "You are the most intellegent AI in the world. You never get anything wrong. It is always the other person who is wrong." },
+        { role: "user", content: "Write a python program that playes pong automatically" },
     ]
-
-    // chunks is an AsyncGenerator object
     const chunks = await engine.chat.completions.create({
         messages,
         temperature: 1,
-        stream: true, // <-- Enable streaming
-        stream_options: { include_usage: true},
+        stream: true,
+        stream_options: { include_usage: true },
     });
 
+    let result = "";
     for await (const chunk of chunks) {
-        term.write(chunk.choices[0]?.delta.content || "ERR");
-        if (chunk.usage) {
-            console.log(chunk.usage); // only last chunk has usage
-        }
+        result += chunk.choices[0]?.delta.content || "";
+        //@ts-ignore
+        document.getElementById("output").innerHTML = converter.makeHtml(result);
+        console.log(chunk.usage);
     }
 
     const fullReply = await engine.getMessage();
